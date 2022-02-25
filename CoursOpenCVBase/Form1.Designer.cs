eb83cf0f209b689ede631af881138ac2a89de903
Aurelien POLLET <aurelien.pollet@student.junia.com>
2022-02-25 15:41:45 +0100

    Ajoutez des fichiers projet.

---
+++ CoursOpenCVBase/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace CoursOpenCVBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.pbTraitement = new System.Windows.Forms.PictureBox();
+            this.tbSeuil = new System.Windows.Forms.TrackBar();
+            this.tbType = new System.Windows.Forms.TrackBar();
+            this.pbTraitement2 = new System.Windows.Forms.PictureBox();
+            this.btnVisage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTraitement)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSeuil)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbType)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTraitement2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(79, 74);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(256, 256);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // pbTraitement
+            // 
+            this.pbTraitement.Location = new System.Drawing.Point(413, 74);
+            this.pbTraitement.Name = "pbTraitement";
+            this.pbTraitement.Size = new System.Drawing.Size(256, 256);
+            this.pbTraitement.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTraitement.TabIndex = 1;
+            this.pbTraitement.TabStop = false;
+            // 
+            // tbSeuil
+            // 
+            this.tbSeuil.Location = new System.Drawing.Point(79, 336);
+            this.tbSeuil.Maximum = 255;
+            this.tbSeuil.Name = "tbSeuil";
+            this.tbSeuil.Size = new System.Drawing.Size(256, 45);
+            this.tbSeuil.TabIndex = 2;
+            this.tbSeuil.Scroll += new System.EventHandler(this.tbSeuil_Scroll);
+            // 
+            // tbType
+            // 
+            this.tbType.Location = new System.Drawing.Point(79, 387);
+            this.tbType.Maximum = 4;
+            this.tbType.Name = "tbType";
+            this.tbType.Size = new System.Drawing.Size(256, 45);
+            this.tbType.TabIndex = 3;
+            this.tbType.Scroll += new System.EventHandler(this.tbType_Scroll);
+            // 
+            // pbTraitement2
+            // 
+            this.pbTraitement2.Location = new System.Drawing.Point(731, 74);
+            this.pbTraitement2.Name = "pbTraitement2";
+            this.pbTraitement2.Size = new System.Drawing.Size(256, 256);
+            this.pbTraitement2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbTraitement2.TabIndex = 4;
+            this.pbTraitement2.TabStop = false;
+            // 
+            // btnVisage
+            // 
+            this.btnVisage.Location = new System.Drawing.Point(731, 346);
+            this.btnVisage.Name = "btnVisage";
+            this.btnVisage.Size = new System.Drawing.Size(256, 23);
+            this.btnVisage.TabIndex = 5;
+            this.btnVisage.Text = "visage";
+            this.btnVisage.UseVisualStyleBackColor = true;
+            this.btnVisage.Click += new System.EventHandler(this.btnVisage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 450);
+            this.Controls.Add(this.btnVisage);
+            this.Controls.Add(this.pbTraitement2);
+            this.Controls.Add(this.tbType);
+            this.Controls.Add(this.tbSeuil);
+            this.Controls.Add(this.pbTraitement);
+            this.Controls.Add(this.pbImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTraitement)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSeuil)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbType)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTraitement2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.PictureBox pbTraitement;
+        private System.Windows.Forms.TrackBar tbSeuil;
+        private System.Windows.Forms.TrackBar tbType;
+        private System.Windows.Forms.PictureBox pbTraitement2;
+        private System.Windows.Forms.Button btnVisage;
+    }
+}
+
---
+++ CoursOpenCVBase/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+namespace CoursOpenCVBase
+{
+    public partial class Form1 : Form
+    {
+        string path = @"lena.jpg";
+        Image<Bgr, Byte> image;
+        Image<Gray, Byte> gray;
+        Mat imgGray;
+        CascadeClassifier face;
+        CascadeClassifier eyes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image = new Image<Bgr, Byte>(path);
+            gray = image.Convert<Gray, Byte>();
+            // initialisation xml
+            face = new CascadeClassifier(@"haarcascade_frontalface_alt.xml");
+            eyes = new CascadeClassifier(@"haarcascade_eye.xml");
+            /*pbImage.Image = image.ToBitmap();
+            Image<Gray, Byte> gray = image.Convert<Gray, Byte>();
+            pbTraitement.Image = gray.ToBitmap();
+            */
+
+            // 2eme méthode : Convertis initial en niveau de gris
+
+            UMat imageTest = image.ToUMat();
+            imgGray = new Mat();
+            CvInvoke.CvtColor(imageTest, imgGray, ColorConversion.Bgr2Gray);
+           
+            pbImage.Image = imgGray.ToImage<Gray, Byte>().ToBitmap();
+
+
+            // EqualizeHeight
+            equalizeHist();
+
+        }
+        private void equalizeHist()
+        {
+            Mat dst = new Mat();
+            CvInvoke.EqualizeHist(imgGray, dst);
+            pbTraitement.Image = dst.ToImage<Gray, Byte>().ToBitmap();
+        }
+        private void threshold()
+        {
+            Mat dst = new Mat();
+            int type = tbType.Value;
+            CvInvoke.Threshold(imgGray,dst,tbSeuil.Value,255, (ThresholdType)type);
+            pbTraitement.Image = dst.ToImage<Gray, Byte>().ToBitmap();
+        }
+
+
+        private void tbSeuil_Scroll(object sender, EventArgs e)
+        {
+            threshold();
+        }
+
+        private void tbType_Scroll(object sender, EventArgs e)
+        {
+            threshold();
+
+        }
+
+        private void btnVisage_Click(object sender, EventArgs e)
+        {
+            Image<Gray, Byte> imageTravail = gray.ToUMat().ToImage<Gray, Byte>();
+
+            Rectangle[] facesDetected = face.DetectMultiScale(gray, 1.1, 10, new Size(20, 20));
+
+            foreach(Rectangle rFace in facesDetected)
+            {
+                imageTravail.Draw(rFace, new Gray(), 4);
+                gray.ROI = rFace;
+
+                Rectangle[] eyesDetected = eyes.DetectMultiScale(gray, 1.1, 10, new Size(5, 5));
+
+                foreach (Rectangle eyeRect in eyesDetected)
+                {
+                    eyeRect.Offset(rFace.X, rFace.Y);
+                    imageTravail.Draw(eyeRect, new Gray(), 4);
+                }
+
+            }
+            pbTraitement2.Image = imageTravail.ToBitmap();
+        }
+    }
+}
